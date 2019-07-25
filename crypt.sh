3c609aa592e89a90b4399d93627dd663f8f79a29
yinxulai <yinxulai@hotmail.com>
2019-07-25 19:08:49 +0800

    添加一键部署脚本

--- crypt.sh
+++ crypt.sh
@@ -1,17 +1,21 @@
 #! /bin/bash
 
+# 使用说明
+# ./crypt.sh -d k iv 一键解密
+# ./crypt.sh -e k iv 一键加密
+# k iv 加密解密用到的密钥
+
 for file in `ls`; do
   if [ -f $file ]; then
     fileName=${file%.*};
     fileFormat=${file##*.};
-    echo $fileFormat $fileName;
 
-      if [ $1 = "d" ]; then
+      if [ $1 = "-d" ]; then
         if [ $fileFormat = "enc" ]; then
           echo "Dncrypt file $file to $fileName";
           openssl enc -aes-256-cbc -d -K $2 -iv $3 -in ./$file -out ./$fileName;
         fi
-      elif [ $1 = "e" ]; then
+      elif [ $1 = "-e" ]; then
         if [ $fileFormat = "key" ]; then
           echo "Encrypt file $file to $fileName";
           openssl enc -aes-256-cbc -K $2 -iv $3 -in ./$file -out ./$file.enc;
---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+
+# 复制.key 文件到
+# 添加 key config
+# 使用管理员权限运行
+# key 目前仅支持放在脚本同级目录中
+# TODO 目前追加写入配置 可能会有重复
+# 脚本使用 ./deploy.sh name root 192.168.0.1 22 ./ssh.key
+# 参数说明 ./deploy.sh 别名 #user #服务器  #端口  #证书
+# 部署完 ssh 可以使用别名来进行登陆: ssh name name 为别名
+
+sshDir="./test";
+
+if [ -f $5 ]; then
+    if [ ! -e $sshDir ]; then
+      mkdir $sshDir;
+    fi
+
+    yes | cp $5 $sshDir/$5;
+    sudo chmod 644 $sshDir/$5
+    echo "HostName $1" >> $sshDir/config
+    echo "    User $2" >> $sshDir/config
+    echo "    Host $3" >> $sshDir/config
+    echo "    Port $4" >> $sshDir/config
+    echo "    IdentityFile $5" >> $sshDir/config
+fi
