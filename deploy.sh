1b12f3a88065bfbe459364f1e6a87d99ce02dd24
yinxulai <yinxulai@hotmail.com>
2019-07-26 09:54:40 +0800

    调试脚本

--- deploy.sh
+++ deploy.sh
@@ -9,19 +9,25 @@
 # 参数说明 ./deploy.sh 别名 #user #服务器  #端口  #证书
 # 部署完 ssh 可以使用别名来进行登陆: ssh name name 为别名
 
-sshDir="./test";
+sshDir="${HOME}/.ssh";
 
 if [ -f $5 ]; then
-    if [ ! -e $sshDir ]; then
-      mkdir $sshDir;
+    if [ ! -d $sshDir ]; then
+      echo  "mkdir $sshDir folder"
+      mkdir -p $sshDir;
+    fi
+    if [ ! -f $sshDir/config ]; then
+      echo  "touch $sshDir/config file"
+      touch $sshDir/config;
     fi
 
-    yes | cp $5 $sshDir/$5;
+    cp $5 $sshDir/$5;
+    echo "$5 $sshDir/$5"
     sudo chmod 600 $sshDir/$5
     echo "Host $1" >> $sshDir/config
     echo "    User $2" >> $sshDir/config
     echo "    Port $4" >> $sshDir/config
     echo "    HostName $3" >> $sshDir/config
-    echo "    IdentityFile $5" >> $sshDir/config
+    echo "    IdentityFile $sshDir/$5" >> $sshDir/config
     echo "    StrictHostKeyChecking no" >> $sshDir/config
 fi
